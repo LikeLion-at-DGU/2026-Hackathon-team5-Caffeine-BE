b5021fa8738f08cecaf32d11db5beb6d466e86e5
choosooa <josuah5160@dgu.ac.kr>
2026-08-16 20:51:43 +0900

    view+serializer+url

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from businesses.models import Business
+from .models import Report
+from .serializers import ReportSerializer
 
-# Create your views here.
+
+def get_current_business():
+    return get_object_or_404(Business, pk=1)
+
+
+class ReportDetailView(APIView):
+    def get(self, request, year_month):
+        business = get_current_business()
+        report = get_object_or_404(Report, business=business, year_month=year_month)
+        return Response(ReportSerializer(report).data)
+
+
+class ReportGenerateView(APIView):
+    def post(self, request, year_month):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+
+
+class ReportDownloadView(APIView):
+    def get(self, request, year_month):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+
+
+class ReportApproveView(APIView):
+    def post(self, request, year_month):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
+
+
+class ReportSendEmailView(APIView):
+    def post(self, request, year_month):
+        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
