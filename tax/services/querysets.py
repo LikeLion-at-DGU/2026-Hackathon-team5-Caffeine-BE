dc1b70344944fd57daf0262f9402d10582537be9
choosooa <josuah5160@dgu.ac.kr>
2026-08-17 21:58:48 +0900

    파일 제거 완료

--- tax/services/querysets.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility imports; effective transaction selection belongs to transactions."""
-
-from transactions.services.querysets import (
-    effective_purchase_transactions,
-    effective_transactions,
-)
-
-
-__all__ = ["effective_purchase_transactions", "effective_transactions"]
